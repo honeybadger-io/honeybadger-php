d8607216f5067a03dd655b84ba7f7329f96442d2
Gabriel Evans <gabriel@codeconcoction.com>
2013-03-29 04:28:06 -0700

    Implement backtrace parser

---
+++ lib/Honeybadger/Backtrace.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Honeybadger;
+
+use \Honeybadger\Backtrace\Line;
+
+/**
+ * Parses and represents backtraces of PHP exceptions for notices.
+ *
+ * @package   Honeybadger
+ * @category  Backtrace
+ */
+class Backtrace {
+
+	/**
+	 * @var  array  List of attributes that can be read through [__get].
+	 */
+	protected static $attributes = array('lines', 'application_lines');
+
+	/**
+	 * @var  array  Holder for an array of [Backtrace\Line]s.
+	 */
+	protected $lines = array();
+
+	/**
+	 * @var  array  Holder for an array of app-specific [Backtrace\Line]s.
+	 */
+	protected $application_lines = array();
+
+	/**
+	 * Parses a PHP backtrace and returns a new `Backtrace` object. Provided
+	 * options are passed to [Line::parse] which may include filters
+	 * (see [Config::$backtrace_filters]) which are called for with each line
+	 * in the trace.
+	 *
+	 * @param   array  $backtrace  The raw PHP backtrace.
+	 * @param   array  $options    Options and filters to apply to lines.
+	 * @return  Backtrace          The parsed backtrace.
+	 */
+	public static function parse(array $backtrace, array $options = array())
+	{
+		$lines = array();
+
+		// Parse each line in the backtrace.
+		foreach ($backtrace as $line)
+		{
+			$parsed = Line::parse($line, $options);
+
+			if ($parsed !== NULL)
+				$lines[] = $parsed;
+		}
+
+		// Instantiate a new backtrace from the lines
+		return new self($lines);
+	}
+
+	/**
+	 * Instantiates a new `Backtrace` with the supplied lines.
+	 *
+	 * @param  array  $lines  Backtrace lines.
+	 */
+	public function __construct(array $lines = array())
+	{
+		$this->lines = $lines;
+
+		foreach ($lines as $line)
+		{
+			if ( ! $line->is_application())
+				continue;
+
+			$this->application_lines[] = $line;
+		}
+	}
+
+	/**
+	 * Provides read-only access to the backtrace's lines.
+	 *
+	 * @param   string  $attr   Attribute to read.
+	 * @return  mixed   The requested property.
+	 */
+	public function __get($attr)
+	{
+		if (in_array($attr, self::$attributes))
+		{
+			return $this->$attr;
+		}
+		else
+		{
+			throw new \Exception('Unknown property '.$attr);
+		}
+	}
+
+	/**
+	 * Throws exceptions (properties are read-only).
+	 *
+	 * @param   string  $attr   Attribute to set.
+	 * @param   mixed   $value  Value to set.
+	 */
+	public function __set($attr, $value)
+	{
+		if (in_array($attr, self::$attributes))
+		{
+			throw new \Exception('Properties are read-only');
+		}
+		else
+		{
+			throw new \Exception('Unknown property '.$attr);
+		}
+	}
+
+	/**
+	 * Formats the backtrace as a string, similar to the format of a typical
+	 * Ruby backtrace (mostly for compatability).
+	 *
+	 * @return  string  The backtrace as a string.
+	 */
+	public function __toString()
+	{
+		return implode("\n", array_map(function($line) {
+			return (string) $line;
+		}, $this->lines));
+	}
+
+	/**
+	 * Formats the backtrace as an array.
+	 *
+	 * @return  array  The backtrace lines.
+	 */
+	public function to_array()
+	{
+		return array_map(function($line) {
+			return $line->to_array();
+		}, $this->lines);
+	}
+
+	/**
+	 * Formats the backtrace as a JSON array.
+	 *
+	 * @return  string  The backtrace lines.
+	 */
+	public function to_json()
+	{
+		return json_encode($this->to_array());
+	}
+
+} // End Backtrace
