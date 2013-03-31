69d2d1e5393d74a7c611647380a3f3b87dcefe57
Gabriel Evans <gabriel@codeconcoction.com>
2013-03-30 21:15:00 -0700

    Add Environment class for handling `$_SERVER`

---
+++ lib/Honeybadger/Environment.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Honeybadger;
+
+use \Honeybadger\Util\Arr;
+use \Honeybadger\Util\SemiOpenStruct;
+use \Honeybadger\Errors\NonExistentProperty;
+use \Honeybadger\Errors\ReadOnly;
+
+/**
+ * Retrieves, stores, and normalizes environment data from `$_SERVER` to prepare
+ * information for serialization in [Notice]s. Additionally, provides
+ * convenience methods for determining the URL of the request trigging an error.
+ */
+class Environment implements \ArrayAccess, \IteratorAggregate {
+
+	public static function factory($data = NULL)
+	{
+		return new self($data);
+	}
+
+	private $_attribute_methods = array(
+		'protocol', 'host', 'port', 'fullpath', 'url',
+	);
+
+	private $data = array();
+
+	private function __construct($data = NULL)
+	{
+		if ($data === NULL)
+		{
+			$data = $_SERVER;
+		}
+
+		$this->data = $data;
+	}
+
+	public function protocol()
+	{
+		return (empty($this['HTTPS']) OR $this['HTTPS'] == 'off') ? 'http' : 'https';
+	}
+
+	public function is_secure()
+	{
+		return ($this->protocol() === 'https');
+	}
+
+	public function host()
+	{
+		return (empty($this['HTTP_HOST'])) ? $this['SERVER_NAME'] : $this['HTTP_HOST'];
+	}
+
+	public function port()
+	{
+		if (empty($this['SERVER_PORT']))
+		{
+			return $this->is_secure() ? 443 : 80;
+		}
+		else
+		{
+			return $this['SERVER_PORT'];
+		}
+	}
+
+	public function is_non_standard_port()
+	{
+		if ($this->is_secure())
+		{
+			return ($this->port() != 443);
+		}
+		else
+		{
+			return ($this->port() != 80);
+		}
+	}
+
+	/**
+	 * Attempts to detect the full URL of the request.
+	 */
+	public function fullpath()
+	{
+		$uri = $this['REQUEST_URI'] ?: $this['PATH_INFO'];
+		$uri = preg_replace('/\?.*$/i', '', $uri);
+		$uri = '/'.ltrim($uri, '/');
+
+		if ( ! empty($this['QUERY_STRING']))
+		{
+			$uri .= '?'.$this['QUERY_STRING'];
+		}
+
+		return $uri;
+	}
+
+	public function url()
+	{
+		if (isset($this->data['url']) AND ! empty($this->data['url']))
+			return $this->data['url'];
+
+		$url = $this->protocol.'://'.$this->host;
+
+		if ($this->is_non_standard_port())
+		{
+			$url .= ':'.$this->port;
+		}
+
+		return $url.$this->fullpath;
+	}
+
+	public function as_array()
+	{
+		foreach ($this->_attribute_methods as $method)
+		{
+			$array[$method] = $this->$method();
+		}
+
+		return $array;
+	}
+
+	public function as_json()
+	{
+		return $this->as_array();
+	}
+
+	public function to_json()
+	{
+		return json_encode($this->as_array());
+	}
+
+	public function __get($key)
+	{
+		if (in_array($key, $this->_attribute_methods))
+		{
+			return $this->$key();
+		}
+		else
+		{
+			throw new NonExistentProperty($this, $key);
+		}
+	}
+
+	public function offsetGet($key)
+	{
+		if (array_key_exists($key, $this->data))
+		{
+			return $this->data[$key];
+		}
+		elseif (in_array($key, $this->_attribute_methods))
+		{
+			return $this->$key();
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+
+	public function offsetSet($key, $value)
+	{
+		throw new ReadOnly($this);
+	}
+
+	public function offsetExists($key)
+	{
+		return (array_key_exists($key, $this->data) OR
+		        in_array($key, $this->_attribute_methods));
+	}
+
+	public function offsetUnset($key)
+	{
+		throw new ReadOnly($this);
+	}
+
+	public function getIterator()
+	{
+		return new \ArrayIterator($this->data);
+	}
+
+}
---
+++ tests/Honeybadger/EnvironmentTest.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Honeybadger;
+
+/**
+ * Tests Honeybadger\Environment.
+ *
+ * @group honeybadger
+ */
+class EnvironmentTest extends \PHPUnit_Framework_TestCase {
+
+	public function test_factory_should_return_instance_of_environment()
+	{
+		$this->assertTrue(Environment::factory() instanceof Environment);
+	}
+
+	public function test_should_use_server_superglobal_when_not_supplied_data()
+	{
+		$environment1 = Environment::factory();
+		$environment2 = Environment::factory($_SERVER);
+
+		$this->assertEquals($environment1, $environment2);
+	}
+
+	public function test_protocol_should_be_http_when_https_blank()
+	{
+		$this->assertEquals('http', Environment::factory(array())->protocol());
+	}
+
+	public function test_protocol_should_be_http_when_https_off()
+	{
+		$this->assertEquals('http', Environment::factory(array(
+			'HTTPS' => 'off',
+		))->protocol());
+	}
+
+	public function test_protocol_should_be_https_when_https_on()
+	{
+		$this->assertEquals('https', Environment::factory(array(
+			'HTTPS' => 'on',
+		))->protocol());
+	}
+
+	public function provider_https_on()
+	{
+		return array(
+			array(
+				'always',
+			),
+			array(
+				'sometimes',
+			),
+			array(
+				'never',
+			),
+			array(
+				'whenever',
+			),
+			array(
+				'maybe',
+			),
+			array(
+				'mostly',
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provider_https_on
+	 */
+	public function test_protocol_should_be_https_when_https_not_blank($value)
+	{
+		$this->assertEquals('https', Environment::factory(array(
+			'HTTPS' => $value,
+		))->protocol());
+	}
+
+	public function test_is_secure()
+	{
+		$this->assertTrue(Environment::factory(array(
+			'HTTPS' => 'on',
+		))->is_secure());
+
+		$this->assertFalse(Environment::factory(array(
+			'HTTPS' => 'off',
+		))->is_secure());
+	}
+
+	public function test_host_uses_server_name_when_http_host_unavailable()
+	{
+		$this->assertEquals('example.com', Environment::factory(array(
+			'SERVER_NAME' => 'example.com',
+		))->host());
+	}
+
+	public function test_host_prefers_http_host()
+	{
+		$this->assertEquals('foo.net', Environment::factory(array(
+			'SERVER_NAME' => 'example.com',
+			'HTTP_HOST'   => 'foo.net',
+		))->host());
+	}
+
+	public function test_port_should_return_server_port()
+	{
+		$this->assertEquals('123', Environment::factory(array(
+			'SERVER_PORT' => '123',
+		))->port());
+	}
+
+	public function test_port_should_detect_default_when_missing()
+	{
+		$this->assertEquals(80, Environment::factory(array(
+			'HTTPS'       => 'off',
+		))->port());
+
+		$this->assertEquals(443, Environment::factory(array(
+			'HTTPS' => 'on',
+		))->port());
+	}
+
+	public function test_non_standard_port_when_ssl()
+	{
+		$this->assertTrue(Environment::factory(array(
+			'HTTPS'       => 'on',
+			'SERVER_PORT' => 123,
+		))->is_non_standard_port());
+
+		$this->assertFalse(Environment::factory(array(
+			'HTTPS'       => 'on',
+			'SERVER_PORT' => 443,
+		))->is_non_standard_port());
+	}
+
+	public function test_non_standard_port_when_http()
+	{
+		$this->assertTrue(Environment::factory(array(
+			'HTTPS'       => 'off',
+			'SERVER_PORT' => 456,
+		))->is_non_standard_port());
+
+		$this->assertFalse(Environment::factory(array(
+			'HTTPS'       => 'off',
+			'SERVER_PORT' => 80,
+		))->is_non_standard_port());
+	}
+
+	public function test_url_uses_environment_when_present()
+	{
+		$env = Environment::factory(array(
+			'url' => 'http://example.com/',
+		));
+
+		$this->assertEquals('http://example.com/', $env['url']);
+	}
+
+	public function test_url_returns_combined_protocol_host_uri_query_string()
+	{
+		$env = Environment::factory(array(
+			'REQUEST_URI'  => '/foo/bar/xyz?one=1&two=2&three=3',
+			'SCRIPT_NAME'  => '/foo/index.php',
+			'HTTPS'        => 'on',
+			'HTTP_HOST'    => 'www.example.com',
+			'QUERY_STRING' => 'one=1&two=2&three=3',
+		));
+
+		$this->assertEquals('https://www.example.com/foo/bar/xyz?one=1&two=2&three=3', $env['url']);
+	}
+
+	public function test_url_adds_port_when_non_standard()
+	{
+		$env = Environment::factory(array(
+			'REQUEST_URI'  => '/foo/bar/xyz?one=1&two=2&three=3',
+			'SCRIPT_NAME'  => '/foo/index.php',
+			'HTTPS'        => '',
+			'HTTP_HOST'    => 'www.example.com',
+			'QUERY_STRING' => 'one=1&two=2&three=3',
+			'SERVER_PORT'  => '123',
+		));
+
+		$this->assertEquals('http://www.example.com:123/foo/bar/xyz?one=1&two=2&three=3', $env['url']);
+	}
+
+}
