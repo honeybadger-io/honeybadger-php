a619424e856c6c3d0b764acb0647708dd15e147c
Gabriel Evans <gabriel@codeconcoction.com>
2013-04-01 16:53:57 -0700

    Add example slim app

---
+++ examples/slim/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use \Slim\Slim;
+
+// Load our shared Honeybadger config.
+$options = include __DIR__.'/../config.php';
+
+// Create a Slim app with a few settings.
+$app = new Slim(array(
+    'debug' => TRUE,
+    'mode'  => 'examples',
+));
+
+// Configure Honeybadger to integrate with our app.
+Honeybadger\Integrations\Slim::init($app, array(
+	'api_key'           => $options['api_key'],
+	'http_open_timeout' => 15,
+	'http_read_timeout' => 15,
+	'debug'             => TRUE,
+	'project_root'      => realpath(__DIR__),
+));
+
+// Make some routes:
+
+$app->get('/', function() {
+	echo sprintf('<a href="%s/%s">%s</a>', '/slim/fail', rand(0, 999999),
+		'trigger an error');
+});
+
+$app->get('/fail/:id', function($id) {
+	throw new Exception('bleh! '.$id);
+});
+
+// Run the app.
+$app->run();
