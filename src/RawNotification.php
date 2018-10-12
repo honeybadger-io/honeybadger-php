3d0a071ac36b5638fb0ed4e55256d6774e58402f
sixlive <tj@tjmiller.co>
2018-10-12 16:44:25 -0400

    Add raw notification method for fully customized payloads

---
+++ src/RawNotification.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Honeybadger;
+
+use InvalidArgumentException;
+use Honeybadger\Support\Repository;
+
+class RawNotification
+{
+    /**
+     * @var \Honeybadger\Config
+     */
+    protected $config;
+
+    /**
+     * @var \Honeybadger\Support\Repository
+     */
+    protected $context;
+
+    /**
+     * @param  \Honeybadger\Config  $config
+     * @param  \Honeybadger\Support\Repository  $context
+     */
+    public function __construct(Config $config, Repository $context)
+    {
+        $this->config = $config;
+        $this->context = $context;
+    }
+
+    /**
+     * @param  array  $payload
+     * @return array
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function make(array $payload) : array
+    {
+        $payload = array_merge(
+            [],
+            ['notifier' => $this->config['notifier']],
+            ['error' => []],
+            ['request' => ['context' => (object) $this->context->all()],
+            ],
+            ['server' => (object) []],
+            $payload
+        );
+
+        $this->validatePayload($payload);
+
+        return $payload;
+    }
+
+    /**
+     * @param  array  $payload
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    private function validatePayload(array $payload) : void
+    {
+        if (empty($payload['error']['class'])) {
+            throw new InvalidArgumentException('The notification error.class field is required');
+        }
+
+        if (empty($payload['notifier']['name'])) {
+            throw new InvalidArgumentException('The notification notifier.name field is required');
+        }
+    }
+}
