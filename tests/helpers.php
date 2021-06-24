dc53b74e75ec39c2951a851ad24791333e78eaca
shalvah <diakon.ng@gmail.com>
2021-06-24 19:38:08 +0100

    Fix style

--- tests/helpers.php
+++ tests/helpers.php
@@ -7,7 +7,6 @@ function array_only($array, $keys)
     return array_intersect_key($array, array_flip((array) $keys));
 }
 
-
 function array_except($array, $keys)
 {
     return array_diff_key($array, array_flip((array) $keys));
