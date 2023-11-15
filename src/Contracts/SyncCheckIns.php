fe5bd9ba828bdec2d568df342024ab63877d5d0f
Pangratios Cosma <pagcosma@gmail.com>
2023-11-15 10:47:36 +0200

    chore(check-ins): more

--- src/Contracts/SyncCheckIns.php
+++ src/Contracts/SyncCheckIns.php
@@ -7,13 +7,13 @@ use Honeybadger\CheckIn;
 interface SyncCheckIns
 {
     /**
-     * Given an array of checkin definitions,
+     * Given an array of check-in definitions,
      * create, update, or delete them as necessary.
      *
-     * The return value is an array of checkins that were created, updated or deleted.
+     * The return value is an array of check-ins that were created, updated or deleted.
      *
      * @param array $checkIns
-     * @return CheckIn[] An array of checkins that were created, updated or deleted.
+     * @return CheckIn[] An array of check-ins that were created, updated or deleted.
      */
     public function sync(array $checkIns): array;
 }
